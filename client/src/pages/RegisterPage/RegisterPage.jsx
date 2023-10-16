0b690a757a88dc6a87a06d8e27f63381582ac6fd
Bez1887 <behzadpaki@gmail.com>
2023-10-16 13:14:13 +0200

    Create register component

---
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,17 @@
+import Logo from "../../components/Logo/Logo.jsx";
+
+
+const RegisterPage = () => {
+    return (
+        <div className="registerpage">
+            <Logo/>
+        </div>
+    )
+
+}
+
+
+
+
+
+export default RegisterPage;
