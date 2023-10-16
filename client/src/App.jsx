f3f025e50b86c35ffdcb66e170e3d0590431bdfe
Jules <110929073+juse122@users.noreply.github.com>
2023-10-16 12:03:39 +0200

    Add React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage.jsx";
+import RegisterPage from "./pages/RegisterPage/RegisterPage.jsx";
 import './App.scss';
 
 const App = () => {
     return (
     <div className="App">
-        <LandingPage />
+
+        <HashRouter>
+            <div className="page">
+                <Routes>
+                    <Route path="/" element={ <LandingPage /> } />
+                    <Route path="/register" element={ <RegisterPage /> } />
+                    {/* <Route path="/login" element={ <Login /> } />
+                    <Route path="/collections" element={ <MyCollections /> } />
+                    <Route path="*" element={ <NotFound /> } /> */}
+                </Routes>
+            </div>
+        </HashRouter>
+
     </div>
     )
 };
