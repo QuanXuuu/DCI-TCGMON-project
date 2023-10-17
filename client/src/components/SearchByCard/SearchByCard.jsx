807452cf8d01108a455706f8be4b4a50a2695da9
Bez1887 <behzadpaki@gmail.com>
2023-10-17 14:51:50 +0200

    Create search by set & search by card

---
+++ client/src/components/SearchByCard/SearchByCard.jsx
@@ -0,0 +1,17 @@
+import Button from "../../components/Button/Button"
+import './SearchByCard.scss'; 
+
+const SearchByCard = () => {
+    return (
+        <div className="SearchByCard">
+            <div className="SearchByCardForm">
+            <form action="">
+                <input type="text" placeholder='Card name' />
+            </form>
+           </div>
+            <Button text={ "Show results" } />
+        </div>
+    )
+}
+
+export default SearchByCard;
--- client/src/pages/SearchPage/SearchPage.jsx
+++ client/src/pages/SearchPage/SearchPage.jsx
@@ -1,4 +1,5 @@
 import Header from "../../components/Header/Header";
+import SearchByCard from "../../components/SearchByCard/SearchByCard";
 import './SearchPage.scss'; 
 
 const SearchPage = () => {
@@ -11,6 +12,7 @@ const SearchPage = () => {
                 <button className="SwitchButton">Search by set</button>
                 <button className="SwitchButton">Search by card</button>
             </div>
+            <SearchByCard/>
             </div>
         </div>
         
