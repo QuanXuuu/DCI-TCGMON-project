79b69af684fe2b7c8590ad359ecd00a52fad0307
Quan Xu <xuquan69220@gmail.com>
2023-10-19 13:52:20 +0200

    Add CollectionDetailsPage

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,16 +1,14 @@
-import "./LoginForm.scss";
+import './LoginForm.scss';
 
 const LoginForm = () => {
-    return (
-        <div className="LoginForm">
-               
-                <form action="">
-                    <input type="email" placeholder="Email address"/>
-                <input type="password" placeholder="Password" />
-                </form>
-            </div>
-        
-    )
-}
+  return (
+    <div className="LoginForm">
+      <form action="">
+        <input type="email" placeholder="Email address" />
+        <input type="password" placeholder="Password" />
+      </form>
+    </div>
+  );
+};
 
 export default LoginForm;
