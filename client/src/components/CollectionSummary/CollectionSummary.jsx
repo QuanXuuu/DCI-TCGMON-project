76e402622ef109ca338f92548e55670385f5279f
Jules <110929073+juse122@users.noreply.github.com>
2023-10-20 13:15:36 +0200

    Update collection summary positioning and box shadow

--- client/src/components/CollectionSummary/CollectionSummary.jsx
+++ client/src/components/CollectionSummary/CollectionSummary.jsx
@@ -10,27 +10,27 @@ const CollectionSummary = () => {
         <img src={Logo} alt="tcg-logo" />
       </div>
       <div className="collection-title-wrapper">
-        <h2>Ganz tolle Kollektion</h2>
+        <h2>Collection #1</h2>
       </div>
       <div className="card-count-wrapper">
+        <p className="card-count">12.000</p>
         <div className="icon-wrapper">
           <FontAwesomeIcon icon={faCopy} />
         </div>
-        <p className="card-count">12.000</p>
       </div>
       <div className="product-count-wrapper">
+        <p className="product-count">12.000</p>
         <div className="icon-wrapper">
           <FontAwesomeIcon icon={faCube} />
         </div>
-        <p className="product-count">12.000</p>
       </div>
       <div className="collection-value-wrapper">
-        <div className="icon-wrapper">
-          <FontAwesomeIcon icon={faCoins} />
-        </div>
         <p className="collection-value">
           1.001.280<span>€</span>
         </p>
+        <div className="icon-wrapper">
+          <FontAwesomeIcon icon={faCoins} />
+        </div>
       </div>
       <button className="collection-details-link">View collection</button>
     </div>
