763990b24e26c19318ededc1fccf3587921e4f44
RubenKanibal <rubenleon92@web.de>
2023-10-24 10:46:51 +0200

    Add more styles

--- client/src/components/EditCollectionModal/EditCollectionModal.jsx
+++ client/src/components/EditCollectionModal/EditCollectionModal.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import CloseButton from '../CloseButton/CloseButton';
 import AddCollectionButton from '../AddCollectionButton/AddCollectionButton';
 import './EditCollectionModal.scss';
@@ -6,6 +7,10 @@ const EditCollectionModal = ({
   isEditCollectionModalOpen,
   toggleEditCollectionModal,
 }) => {
+
+  const [collectionName, setCollectionName] = useState('');
+  const [collectionTCG, setCollectionTCG] = useState('');
+
   return (
     <div className="EditCollectionModal">
       <div className="close">
@@ -20,15 +25,26 @@ const EditCollectionModal = ({
           className="CollectionNameInput"
           type="text"
           placeholder="Collection Name"
+          onChange={(e) => {
+            setCollectionName(e.target.value);
+          }}
         />
-        <select name="TCGs" id="TCGname">
+        <select
+          name="TCGs"
+          id="TCGname"
+          onChange={(e) => {
+            setCollectionTCG(e.target.value);
+          }}
+        >
           <option value="">Pokemon</option>
         </select>
 
-        <AddCollectionButton text={'Create new collection'} />
+        <AddCollectionButton text={'Update collection'} />
       </div>
       <div className="Delete-Div">
-        <button className="Delete-Button">Delete collection</button>
+        <button
+          className="Delete-Button">Delete collection
+        </button>
       </div>
     </div>
   );
