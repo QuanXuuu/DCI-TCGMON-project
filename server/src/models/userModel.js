0b54f18769155e844843b8f283e5f4607b2485b2
Quan Xu <xuquan69220@gmail.com>
2023-10-26 10:05:26 +0200

    Add JWT && rename files

---
+++ server/src/models/userModel.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+import crypto from "crypto";
+import dotenv from "dotenv";
+import validator from "validator";
+dotenv.config();
+
+const secret = process.env.SECRET_KEY;
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Please provide your email"],
+      lowercase: true,
+      validate: [validator.isEmail, "Please provide a valid email"],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      select: false,
+    },
+    role: { type: String, default: "Member" },
+    joined: {
+      type: String,
+      default: new Date().toLocaleDateString("fr-CA", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      }),
+    },
+    loggedIn: { type: Boolean, default: false },
+    lastLogin: { type: Date, default: "" },
+    collections: { type: Array, default: [] },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        delete ret.password;
+      },
+    },
+  }
+);
+
+userSchema.methods.encryptPassword = (password) => {
+  const hash = crypto
+    .createHmac("sha256", secret)
+    .update(password)
+    .digest("hex");
+  return hash;
+};
+
+userSchema.methods.comparePassword = function (loginPassword) {
+  if (this.password !== this.encryptPassword(loginPassword)) {
+    return false;
+  }
+
+  return true;
+};
+
+const User = new mongoose.model("User", userSchema, "users");
+
+export default User;
---
+++ server/src/routes/userRoutes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import * as userController from "../controllers/userController.js";
+
+const userRouter = Router();
+
+userRouter
+  .post("/signup", userController.createUser)
+  .post("/login", userController.login)
+  .get("/users/:id", userController.getUser)
+  .get("/users", userController.getAllUsers)
+  .patch("/users/:id", userController.updateUser)
+  .delete("/users/:id", userController.deleteUser)
+  .delete("/users", userController.deleteAllUsers);
+
+export default userRouter;
