a7998a073c161cae089c94abe65a5c62a47aafb2
Jules <110929073+juse122@users.noreply.github.com>
2023-11-20 14:54:36 +0100

    Fix first smaller stuff

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ import cookieParser from "cookie-parser";
 import userRouter from "./src/routes/userRoutes.js";
 import "./src/database/db.js";
 import path from "path";
-import { fileURLToPath } from "url";
+// import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use("/api", userRouter);
 app.use("/", express.static(path.join(__dirname, "../client/dist")));
 app.use("/*", (req, res) =>
-	res.sendFile(__dirname + "../client/dist/index.html")
+	res.sendFile(path.join(__dirname, "../client/dist/index.html"))
 );
 
 app.listen(port, () => console.log("Server is running on port: " + port));
