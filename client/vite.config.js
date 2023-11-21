4815b4f7cf9599a202c7f07926423545cb798947
Jules <110929073+juse122@users.noreply.github.com>
2023-11-21 13:34:02 +0100

    Reverted dev settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
