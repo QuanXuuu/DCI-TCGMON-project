d24c42135fb6c6bdeb3b973ea236880aa705ae8a
Jules <110929073+juse122@users.noreply.github.com>
2023-11-24 14:26:22 +0100

    Update deploy to current dev commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000/',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
